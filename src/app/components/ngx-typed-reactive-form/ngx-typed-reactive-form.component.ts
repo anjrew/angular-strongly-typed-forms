b4d447e2941f4d7f3d05d7e2be6d6d89b01481d8
Andrew <earyzhe@gmail.com>
2020-09-26 13:24:05 +0200

    Straing with the new arrangement of the folders

---
+++ src/app/components/ngx-typed-reactive-form/ngx-typed-reactive-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngx-typed-reactive-form',
+  templateUrl: './ngx-typed-reactive-form.component.html',
+  styleUrls: ['./ngx-typed-reactive-form.component.scss']
+})
+export class NgxTypedReactiveFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
